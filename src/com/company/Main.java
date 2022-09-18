2e44a77562fadfbf9eba2ed717245a784e7b6389
Jallller <cph-jl398@cphbusiness.dk>
2022-09-18 10:57:06 +0200

    indenombygning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,8 +14,10 @@ public class Main {
 //
 //
         String svar;
-        svar = getString("hvad ønsker du ? opret/udskriv");
+
         while (true) {
+            svar = getString("hvad ønsker du ? opret / udskriv / slet / opdater");
+
             switch (svar) {
                 case "opret":
                     indsætNavne();
@@ -23,9 +25,79 @@ public class Main {
                 case "udskriv":
                     udskrivNavne();
                     break;
+                case "slet":
+                    slet();
+                    break;
+                case "opdater":
+                    opdaterNavn();
+                    break;
+                default:
+                    System.out.println("den fangede jeg sgu ikke lige");
             }
         }
 
+
+
+
+    }
+
+    private static void opdaterNavn() {
+        udskrivNavne();
+        String sql = "update  Navne set Navne = ? where idNavne = ?";
+
+        try (Connection con =getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);) {
+//            ps.setString(1, getString("angiv nyt navn"));
+
+            // det er det her jeg søger på.
+            String kundeNavn = getString("angiv et nyt navn");
+            ps.setString(1,kundeNavn);
+            ps.setInt(2, getInt("skriv et tal"));
+
+
+            int res = ps.executeUpdate();    //https://javaconceptoftheday.com/difference-between-executequery-executeupdate-execute-in-jdbc/
+
+            if (res > 0) {
+
+                System.out.println("Kunden med navnet " + "\"" + kundeNavn + "\"" + " er nu blevet opdateret");
+            }
+            else {
+                System.out.println("en kunde med det nr fandtes ikke i listen ");
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        udskrivNavne();
+    }
+
+    private static void slet() {
+        udskrivNavne();
+        String sql = "delete from Navne where navne = ?";
+
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);) {
+
+            String kundeNavn = getString("skriv navnet på den kunde du vil slette");
+            ps.setString(1, kundeNavn);
+
+
+            int res = ps.executeUpdate();       //https://javaconceptoftheday.com/difference-between-executequery-executeupdate-execute-in-jdbc/
+
+            if (res > 0) {
+
+                System.out.println("Kunden med navnet " + "\"" + kundeNavn + "\"" + " er nu blevet slettet");
+            } else {
+                System.out.println("en kunde med navnet " + "\"" + kundeNavn + "\"" + " fandtes ikke i listen");
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        udskrivNavne();
     }
 
     private static void indsætNavne() {
@@ -39,7 +111,7 @@ public class Main {
         ) {
 
             // her klargøres mit prepared statement ved at min parametre indsættes.
-            ps.setString(1,getString("skriv et navn"));
+            ps.setString(1, getString("skriv et navn"));
 
             ps.executeUpdate();    //https://javaconceptoftheday.com/difference-between-executequery-executeupdate-execute-in-jdbc/
 
@@ -51,6 +123,7 @@ public class Main {
             e.printStackTrace();
         }
     }
+
     private static void udskrivNavne() {
         List<String> kundeList = new ArrayList<>();
 
@@ -81,12 +154,13 @@ public class Main {
         }
     }
 
-    public static String getString(String s){
+    public static String getString(String s) {
         System.out.println(s + " : ");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
-    public static Connection getConnection () {
+
+    public static Connection getConnection() {
         Connection connection = null;
         String url = "jdbc:mysql://localhost:3306/demodemo?serverTimezone=CET&useSSL=false";
         String user = "root";
@@ -97,5 +171,17 @@ public class Main {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static int getInt(String s){
+
+        while (true) {
+            try {
+                int ans = Integer.parseInt(getString(s));
+                return ans;
+            } catch (NumberFormatException e) {
+                System.out.println("husk ikke et tal ord");
+            }
+        }
     }
 }
